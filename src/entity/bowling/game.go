9126de6b66a04e462d28501a0a9a8d459430298c
Lex Liu <lexamo.liu@gmail.com>
2021-11-29 00:09:01 +0800

    Replace getStatusBonus method to a private function
    The getStatusBonus method does not use the value of the Bowling object.
    Replace it by a private function for easy testing.

--- src/entity/bowling/game.go
+++ src/entity/bowling/game.go
@@ -27,7 +27,7 @@ func (b *Bowling) Hit(game valueobject.BowlingGame, pins uint32) valueobject.Bow
 	game.ThrowNumber = game.ThrowNumber + 1
 	game.Left = game.Left - pins
 	game.Score = game.Score + pins
-	game.Status, game.ExtraBonus = b.getStatusBonus(game)
+	game.Status, game.ExtraBonus = getStatusBonus(game)
 	if game.WithoutExtraBonus {
 		game.ExtraBonus = 0
 	}
@@ -42,30 +42,30 @@ func bonus(game valueobject.BowlingGame, pins uint32) valueobject.BowlingGame {
 	return game
 }
 
-func (b *Bowling) isStrike(game valueobject.BowlingGame) bool {
+func isStrike(game valueobject.BowlingGame) bool {
 	return game.ThrowNumber == 1 && game.Left == 0
 }
 
-func (b *Bowling) isSpare(game valueobject.BowlingGame) bool {
+func isSpare(game valueobject.BowlingGame) bool {
 	return game.ThrowNumber == 2 && game.Left == 0
 }
 
-func (b *Bowling) gainExtraBonus(game valueobject.BowlingGame, extraBonus uint32) uint32 {
+func gainExtraBonus(game valueobject.BowlingGame, extraBonus uint32) uint32 {
 	if game.WithoutExtraBonus {
 		return 0
 	}
 	return extraBonus
 }
 
-func NoMoreHit(game valueobject.BowlingGame) bool {
-	return game.ThrowNumber == 2 || game.Status == valueobject.Strike
-}
-
-func (b *Bowling) getStatusBonus(game valueobject.BowlingGame) (s valueobject.BowlingGameStatus, extraBonus uint32) {
-	if b.isSpare(game) {
-		return valueobject.Spare, b.gainExtraBonus(game, 1)
-	} else if b.isStrike(game) {
-		return valueobject.Strike, b.gainExtraBonus(game, 2)
+func getStatusBonus(game valueobject.BowlingGame) (s valueobject.BowlingGameStatus, extraBonus uint32) {
+	if isSpare(game) {
+		return valueobject.Spare, gainExtraBonus(game, 1)
+	} else if isStrike(game) {
+		return valueobject.Strike, gainExtraBonus(game, 2)
 	}
 	return valueobject.Open, 0
+}
+
+func NoMoreHit(game valueobject.BowlingGame) bool {
+	return game.ThrowNumber == 2 || game.Status == valueobject.Strike
 }
--- src/entity/bowling/game_internal_test.go
+++ src/entity/bowling/game_internal_test.go
@@ -49,3 +49,213 @@ func Test_bonus(t *testing.T) {
 		})
 	}
 }
+
+func Test_isStrike(t *testing.T) {
+	type args struct {
+		game valueobject.BowlingGame
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "clean with 2 throws",
+			args: args{
+				game: valueobject.BowlingGame{
+					ThrowNumber: 2,
+					Left:        0,
+				},
+			},
+			want: false,
+		},
+		{
+			name: "did not clean the game with 1 throw",
+			args: args{
+				game: valueobject.BowlingGame{
+					ThrowNumber: 1,
+					Left:        1,
+				},
+			},
+		},
+		{
+			name: "clean with 1 throw",
+			args: args{
+				game: valueobject.BowlingGame{
+					ThrowNumber: 1,
+					Left:        0,
+				},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, isStrike(tt.args.game))
+		})
+	}
+}
+
+func Test_isSpare(t *testing.T) {
+	type args struct {
+		game valueobject.BowlingGame
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "clean with 2 throws",
+			args: args{
+				game: valueobject.BowlingGame{
+					ThrowNumber: 2,
+					Left:        0,
+				},
+			},
+			want: true,
+		},
+		{
+			name: "did not clean with 2 throws",
+			args: args{
+				game: valueobject.BowlingGame{
+					ThrowNumber: 2,
+					Left:        1,
+				},
+			},
+			want: false,
+		},
+		{
+			name: "clean with 1 throws",
+			args: args{
+				game: valueobject.BowlingGame{
+					ThrowNumber: 1,
+					Left:        0,
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, isSpare(tt.args.game))
+		})
+	}
+}
+
+func Test_gainExtraBonus(t *testing.T) {
+	type args struct {
+		game       valueobject.BowlingGame
+		extraBonus uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint32
+	}{
+		{
+			name: "game without extra bonus",
+			args: args{
+				game: valueobject.BowlingGame{
+					WithoutExtraBonus: false,
+				},
+				extraBonus: 1,
+			},
+			want: 1,
+		},
+		{
+			name: "game with extra bonus",
+			args: args{
+				game: valueobject.BowlingGame{
+					WithoutExtraBonus: true,
+				},
+				extraBonus: 1,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, gainExtraBonus(tt.args.game, tt.args.extraBonus))
+		})
+	}
+}
+
+func Test_getStatusBonus(t *testing.T) {
+	type args struct {
+		game valueobject.BowlingGame
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantS          valueobject.BowlingGameStatus
+		wantExtraBonus uint32
+	}{
+		{
+			name: "a spare game with extra bonus",
+			args: args{
+				game: valueobject.BowlingGame{
+					ThrowNumber:       2,
+					Left:              0,
+					WithoutExtraBonus: false,
+				},
+			},
+			wantS:          valueobject.Spare,
+			wantExtraBonus: 1,
+		},
+		{
+			name: "a spare game without extra bonus",
+			args: args{
+				game: valueobject.BowlingGame{
+					ThrowNumber:       2,
+					Left:              0,
+					WithoutExtraBonus: true,
+				},
+			},
+			wantS:          valueobject.Spare,
+			wantExtraBonus: 0,
+		},
+		{
+			name: "a strike game with extra bonus",
+			args: args{
+				game: valueobject.BowlingGame{
+					ThrowNumber:       1,
+					Left:              0,
+					WithoutExtraBonus: false,
+				},
+			},
+			wantS:          valueobject.Strike,
+			wantExtraBonus: 2,
+		},
+		{
+			name: "a strike game without extra bonus",
+			args: args{
+				game: valueobject.BowlingGame{
+					ThrowNumber:       1,
+					Left:              0,
+					WithoutExtraBonus: true,
+				},
+			},
+			wantS:          valueobject.Strike,
+			wantExtraBonus: 0,
+		},
+		{
+			name: "a normal game",
+			args: args{
+				game: valueobject.BowlingGame{
+					ThrowNumber: 1,
+					Left:        1,
+				},
+			},
+			wantS:          valueobject.Open,
+			wantExtraBonus: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotS, gotExtraBonus := getStatusBonus(tt.args.game)
+			assert.Equal(t, tt.wantS, gotS)
+			assert.Equal(t, tt.wantExtraBonus, gotExtraBonus)
+		})
+	}
+}
